02337ad5a384a6f0da312277dcb7a86e5530f3bd
TheBartThe <vieran96@gmail.com>
2020-01-09 01:33:45 +0000

    added quotes

---
+++ applicationQuotes/application/getQuote.py
@@ -0,0 +1,14 @@
+quotes = {
+    "I'm afraid I just blue myself": "Tobias",
+    "I've made a huge mistake": "GOB",
+    "I don't know what I expected": "Michael";
+    "The seal is for marksmanship": "Buster",
+    "Hey... where the f*** are my hard-boiled eggs?!": "Tobias",
+    "And that's why you always leave a note": "J. Walter Weatherman",
+    "OH MY GOD! THEY'RE HAVING A FIRE... sale": "Tobias",
+    "Yeah, the guy in the $3,000 suit is holding the elevator for the guy who doesn’t make that in four months. Come on!": "GOB",
+    "I don’t understand the question and I won’t respond to it": "Lucille",
+    "She thinks I’m too critical. That’s another fault of hers": "Lucille",
+    "It's hot ham water": "Lindsay",
+    "I got the worst f***ing attorneys": "George Snr",
+    
